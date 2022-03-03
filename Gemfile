a42bbf4a097140e72b876f4e1aaf82417810428c
namnt9199 <namnt9199@co-well.vn>
2022-03-03 13:48:08 +0700

    confix to deploy

--- Gemfile
+++ Gemfile
@@ -31,6 +31,11 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "capistrano"
+  gem "capistrano3-puma"
+  gem "capistrano-rails", require: false
+  gem "capistrano-bundler", require: false
+  gem "capistrano-rvm"
 end
 
 group :development do
--- config/puma.rb
+++ config/puma.rb
@@ -7,18 +7,22 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3000 }
+app_dir = File.expand_path("../..", __FILE__)
+environment "production"
+
+bind "unix:///home/viet_deploy/deploy/apps/shared/tmp/sockets/puma.sock"
+pidfile '/home/viet_deploy/deploy/apps/shared/tmp/pids/puma.pid'  
+state_path '/home/viet_deploy/deploy/apps/shared/tmp/pids/puma.state'
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
